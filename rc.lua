4241d2ee3bfb5c0200494dfb6fe2ed1c92cae017
rektide <rektide@voodoowarez.com>
2017-04-14 12:59:52 -0400

    Drop titlebar.

--- rc.lua
+++ rc.lua
@@ -468,10 +468,12 @@ awful.rules.rules = {
         }
       }, properties = { floating = true }},
 
-    -- Add titlebars to normal clients and dialogs
-    { rule_any = {type = { "normal", "dialog" }
-      }, properties = { titlebars_enabled = true }
-    },
+    -- -- -- -- awesome-rc-detitlebar:
+    -- -- -- -- Don't do the following, to prevent titlebars from being added to windows:
+    -- -- -- -- -- Add titlebars to normal clients and dialogs
+    -- -- -- -- { rule_any = {type = { "normal", "dialog" }
+    -- -- -- --   }, properties = { titlebars_enabled = true }
+    -- -- -- -- },
 
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
